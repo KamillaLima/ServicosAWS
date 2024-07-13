11fdeaa61dcc33ef1f4f481b6d1d2de7e3c4a6ec
Kamilla Lima <kamillalima868@gmail.com>
2024-07-12 22:10:17 -0300

    First commit

---
+++ api/src/main/java/com/eventostec/api/domain/address/Address.java
@@ -0,0 +1,27 @@
+package com.eventostec.api.domain.address;
+
+import com.eventostec.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name="address")
+@Getter
+@Setter
+public class Address {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name="event_id")
+    private Event event;
+}
